56ab186ee31e15ee437438f5a18242c3954970d7
Himanshu <gltestconsultant@gmail.com>
2021-05-19 17:21:48 +0530

    initial commit

---
+++ SpecFlowProject2/SpecFlowProject2/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,51 @@
+﻿using BoDi;
+using SpecFlowProject2.Hooks;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject2.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+     //   private readonly ScenarioContext _scenarioContext;
+        private readonly WebDriverSupport _WebDriverSupport;
+        public CalculatorStepDefinitions(WebDriverSupport WebDriverSupports)
+        {
+            _WebDriverSupport = WebDriverSupports;          
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _WebDriverSupport.getDriver().Url = "https://www.hindustantimes.com/";
+             
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+           // Thread.Sleep(5000);
+           // _WebDriverSupport.getDriver().Quit(); 
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+
+           // _scenarioContext.Pending();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+
+            //_scenarioContext.Pending();
+        }
+    }
+}
---
+++ SpecFlowProject2/SpecFlowProject2/Steps/LaunchGoogleSteps.cs
@@ -0,0 +1,30 @@
+﻿using SpecFlowProject2.Hooks;
+using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject2.Steps
+{
+    [Binding]
+    public class LaunchGoogleSteps
+    {
+        private readonly WebDriverSupport _WebDriverSupport;
+        public LaunchGoogleSteps(WebDriverSupport WebDriverSupports)
+        {
+            _WebDriverSupport = WebDriverSupports;
+        }
+        
+        [Given(@"User launches google\.com")]
+        public void GivenUserLaunchesGoogle_Com()
+        {
+            _WebDriverSupport.getDriver().Url = "http://www.google.com";
+        }
+        
+        [Then(@"he should be able to launch")]
+        public void ThenHeShouldBeAbleToLaunch()
+        {
+           // Thread.Sleep(5000);
+           // _WebDriverSupport.getDriver().Quit(); 
+        }
+    }
+}
